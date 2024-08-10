b1ce97864083e3366495058acdd1c931e415ee5e
voytas <voytas@outlook.com>
2024-08-10 20:00:29 +0100

    Basic Paused overlay

---
+++ src/Spectral/Controls/PausedOverlay.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace OldBit.Spectral.Controls;
+
+public class PausedOverlay : TemplatedControl
+{
+    // public static readonly StyledProperty<double> FontSizeProperty =
+    //     AvaloniaProperty.Register<PausedOverlay1, double>(nameof(CellHeight), 32);
+    //
+    // public double CellHeight
+    // {
+    //     get => GetValue(FontSizeProperty);
+    //     set => SetValue(FontSizeProperty, value);
+    // }
+}
