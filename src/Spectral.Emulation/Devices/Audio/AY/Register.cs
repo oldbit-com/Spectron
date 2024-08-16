c3f90628f1601bfdc32d85efa22be2a7ab3d5dd5
voytas <voytas@outlook.com>
2024-08-16 18:57:33 +0100

    AY

---
+++ src/Spectral.Emulation/Devices/Audio/AY/Register.cs
@@ -0,0 +1,21 @@
+namespace OldBit.Spectral.Emulation.Devices.Audio.AY;
+
+internal static class Register
+{
+    internal const int FineTuneA = 0;
+    internal const int CoarseTuneA = 1;
+    internal const int FineTuneB = 2;
+    internal const int CoarseTuneB = 3;
+    internal const int FineTuneC = 4;
+    internal const int CoarseTuneC = 5;
+    internal const int NoisePeriod = 6;
+    internal const int Mixer = 7;
+    internal const int AmplitudeA = 8;
+    internal const int AmplitudeB = 9;
+    internal const int AmplitudeC = 10;
+    internal const int EnvelopeFineTune = 11;
+    internal const int EnvelopeCoarseTune = 12;
+    internal const int EnvelopeShape = 13;
+    internal const int IOPortA = 14;
+    internal const int IOPortB = 15;
+}
