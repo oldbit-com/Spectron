4a366a65a2b5b16fd0c2282e49ce738719db4824
voytas <voytas@outlook.com>
2024-09-04 20:18:57 +0100

    Add settings window (wip)

---
+++ src/Spectron/Views/SettingsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OldBit.Spectron.Views;
+
+public partial class SettingsView : Window
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
