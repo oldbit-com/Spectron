e8e00f8bc91e4f52e04494def2fde02556686cef
voytas <voytas@outlook.com>
2024-09-13 21:04:38 +0100

    Split main view model into partials

---
+++ src/Spectron/ViewModels/MainWindowViewModel.Handlers.cs
@@ -0,0 +1,239 @@
+using System;
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Avalonia.Controls;
+using OldBit.Spectron.Dialogs;
+using OldBit.Spectron.Emulation;
+using OldBit.Spectron.Emulation.Devices.Joystick;
+using OldBit.Spectron.Emulation.Rom;
+using OldBit.Spectron.Emulation.Snapshot;
+using OldBit.Spectron.Emulation.Storage;
+using OldBit.Spectron.Emulation.Tape;
+using OldBit.Spectron.Helpers;
+using OldBit.Spectron.Models;
+using OldBit.Spectron.Views;
+
+namespace OldBit.Spectron.ViewModels;
+
+partial class MainWindowViewModel
+{
+    private async Task HandleLoadFileAsync() => await HandleLoadFileAsync(null);
+
+    private async Task HandleLoadFileAsync(string? filePath)
+    {
+        try
+        {
+            Emulator?.Pause();
+
+            if (filePath == null)
+            {
+                var files = await FileDialogs.OpenAnyFileAsync();
+                if (files.Count <= 0)
+                {
+                    return;
+                }
+
+                filePath = files[0].Path.LocalPath;
+            }
+
+            var fileType = FileTypeHelper.GetFileType(filePath);
+            if (fileType.IsSnapshot())
+            {
+                var emulator = _snapshotFile.Load(filePath);
+                InitializeEmulator(emulator);
+            }
+            else
+            {
+                Emulator?.LoadTape(filePath);
+            }
+
+            RecentFilesViewModel.Add(filePath);
+        }
+        catch (Exception ex)
+        {
+            await MessageDialogs.Error(ex.Message);
+            RecentFilesViewModel.Remove(filePath);
+        }
+        finally
+        {
+            Emulator?.Resume();
+        }
+    }
+
+    private async Task HandleSaveFileAsync()
+    {
+        try
+        {
+            Emulator?.Pause();
+
+            var file = await FileDialogs.SaveSnapshotFileAsync();
+            if (file != null && Emulator != null)
+            {
+                SnapshotFile.Save(file.Path.LocalPath, Emulator);
+            }
+        }
+        catch (Exception ex)
+        {
+            await MessageDialogs.Error(ex.Message);
+        }
+        finally
+        {
+            Emulator?.Resume();
+        }
+    }
+
+    private void HandleChangeBorderSize(BorderSize borderSize)
+    {
+        BorderSize = borderSize;
+
+        _frameBufferConverter.SetBorderSize(borderSize);
+        SpectrumScreen = _frameBufferConverter.Bitmap;
+    }
+
+    private void HandleChangeRom(RomType romType)
+    {
+        RomType = romType;
+
+        CreateEmulator();
+    }
+
+    private void HandleChangeComputerType(ComputerType computerType)
+    {
+        ComputerType = computerType;
+
+        CreateEmulator();
+    }
+
+    private void HandleChangeJoystickType(JoystickType joystickType)
+    {
+        JoystickType = joystickType;
+        Emulator?.JoystickManager.SetupJoystick(joystickType);
+    }
+
+    private void HandleToggleUlaPlus()
+    {
+        IsUlaPlusEnabled = !IsUlaPlusEnabled;
+
+        if (Emulator != null)
+        {
+            Emulator.IsUlaPlusEnabled = IsUlaPlusEnabled;
+        }
+    }
+
+    private void HandleMachineReset()
+    {
+        Emulator?.Reset();
+        IsPaused = Emulator?.IsPaused ?? false;
+    }
+
+    private void HandleTogglePause()
+    {
+        switch (Emulator?.IsPaused)
+        {
+            case true:
+                Emulator.Resume();
+                break;
+
+            case false:
+                Emulator.Pause();
+                break;
+        }
+
+        IsPaused = Emulator?.IsPaused ?? false;
+
+        if (IsPaused)
+        {
+            TimeMachineViewModel.BeforeShow();
+        }
+    }
+
+    private void HandleSetEmulationSpeed(string emulationSpeed)
+    {
+        if (!int.TryParse(emulationSpeed, out var emulationSpeedValue))
+        {
+            return;
+        }
+
+        Emulator?.SetEmulationSpeed(emulationSpeedValue);
+        EmulationSpeed = emulationSpeed;
+    }
+
+    private void HandleToggleFullScreen() =>
+        WindowState = WindowState == WindowState.FullScreen ? WindowState.Normal : WindowState.FullScreen;
+
+    private void HandleSetTapeLoadingSpeed(TapeLoadingSpeed tapeLoadingSpeed) => TapeLoadingSpeed = tapeLoadingSpeed;
+
+    private void HandleHelpKeyboardCommand()
+    {
+        if (_helpKeyboardView == null)
+        {
+            _helpKeyboardView = new HelpKeyboardView();
+            _helpKeyboardView.Closed += (_, _) => _helpKeyboardView = null;
+
+            if (MainWindow != null)
+            {
+                _helpKeyboardView.Show(MainWindow);
+            }
+            else
+            {
+                _helpKeyboardView.Show();
+            }
+        }
+        else
+        {
+            _helpKeyboardView.Activate();
+        }
+    }
+
+    private void HandleShowTimeMachineCommand()
+    {
+    }
+
+    private void HandleKeyUp(KeyEventArgs e)
+    {
+        if (IsPaused)
+        {
+            return;
+        }
+
+        if (JoystickType != JoystickType.None)
+        {
+            var joystickKeys = KeyMappings.ToJoystickAction(e);
+            if (joystickKeys != JoystickInput.None)
+            {
+                Emulator?.JoystickManager.HandleInput(joystickKeys, isOn: false);
+                return;
+            }
+        }
+
+        var keys = KeyMappings.ToSpectrumKey(e);
+        Emulator?.KeyboardHandler.HandleKeyUp(keys);
+    }
+
+    private void HandleKeyDown(KeyEventArgs e)
+    {
+        if (IsPaused)
+        {
+            if (e.Key == Key.Escape)
+            {
+                HandleTogglePause();
+            }
+            return;
+        }
+
+        if (JoystickType != JoystickType.None && _useCorsorKeysAsJoystick)
+        {
+            var joystickKeys = KeyMappings.ToJoystickAction(e);
+            if (joystickKeys != JoystickInput.None)
+            {
+                Emulator?.JoystickManager.HandleInput(joystickKeys, isOn: true);
+                return;
+            }
+        }
+
+        var keys = KeyMappings.ToSpectrumKey(e);
+        Emulator?.KeyboardHandler.HandleKeyDown(keys);
+    }
+
+    private void HandleTimeTravel(TimeMachineEntry entry) => CreateEmulator(entry.Snapshot);
+}
--- src/Spectron/ViewModels/MainWindowViewModel.Properties.cs
+++ src/Spectron/ViewModels/MainWindowViewModel.Properties.cs
@@ -1,6 +1,90 @@
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using OldBit.Spectron.Emulation;
+using OldBit.Spectron.Emulation.Devices.Joystick;
+using OldBit.Spectron.Emulation.Rom;
+using OldBit.Spectron.Emulation.Tape;
+using OldBit.Spectron.Models;
+using ReactiveUI;
+
 namespace OldBit.Spectron.ViewModels;
 
 partial class MainWindowViewModel
 {
+    private BorderSize _borderSize = BorderSize.Medium;
+    public BorderSize BorderSize
+    {
+        get => _borderSize;
+        set => this.RaiseAndSetIfChanged(ref _borderSize, value);
+    }
 
+    private RomType _romType = RomType.Original;
+    public RomType RomType
+    {
+        get => _romType;
+        set => this.RaiseAndSetIfChanged(ref _romType, value);
+    }
+
+    private ComputerType _computerType = ComputerType.Spectrum48K;
+    public ComputerType ComputerType
+    {
+        get => _computerType;
+        set => this.RaiseAndSetIfChanged(ref _computerType, value);
+    }
+
+    private JoystickType _joystickType = JoystickType.None;
+    public JoystickType JoystickType
+    {
+        get => _joystickType;
+        set => this.RaiseAndSetIfChanged(ref _joystickType, value);
+    }
+
+    private bool _isUlaPlusEnabled;
+    public bool IsUlaPlusEnabled
+    {
+        get => _isUlaPlusEnabled;
+        set => this.RaiseAndSetIfChanged(ref _isUlaPlusEnabled, value);
+    }
+
+    private WriteableBitmap? _spectrumScreen;
+    public WriteableBitmap? SpectrumScreen
+    {
+        get => _spectrumScreen;
+        set => this.RaiseAndSetIfChanged(ref _spectrumScreen, value);
+    }
+
+    private bool _isPaused;
+    public bool IsPaused
+    {
+        get => _isPaused;
+        set => this.RaiseAndSetIfChanged(ref _isPaused, value);
+    }
+
+    private string _emulationSpeed = "100";
+    public string EmulationSpeed
+    {
+        get => _emulationSpeed;
+        set => this.RaiseAndSetIfChanged(ref _emulationSpeed, value);
+    }
+
+    private WindowState _windowState = WindowState.Normal;
+    public WindowState WindowState
+    {
+        get => _windowState;
+        set => this.RaiseAndSetIfChanged(ref _windowState, value);
+    }
+
+    private string _windowStateCommandName = string.Empty;
+    public string WindowStateCommandName
+    {
+        get => _windowStateCommandName;
+        set => this.RaiseAndSetIfChanged(ref _windowStateCommandName, value);
+    }
+
+    private TapeLoadingSpeed _tapeLoadingSpeed = TapeLoadingSpeed.Instant;
+    public TapeLoadingSpeed TapeLoadingSpeed
+    {
+        get => _tapeLoadingSpeed;
+        set => this.RaiseAndSetIfChanged(ref _tapeLoadingSpeed, value);
+    }
 }
