c6d89c2cb6a180f08e454ec25661bac45a57c28c
voytas <voytas@outlook.com>
2024-11-04 20:00:07 +0000

    Undo buffer changes

--- src/Spectron.Emulation/Devices/Audio/AudioBuffer.cs
+++ src/Spectron.Emulation/Devices/Audio/AudioBuffer.cs
@@ -2,27 +2,22 @@ namespace OldBit.Spectron.Emulation.Devices.Audio;
 
 internal sealed class AudioBuffer
 {
-   // private readonly List<byte> _buffer = [];
-    private byte[] _buffer = [];
+    private readonly List<byte> _buffer = [];
     private int _count;
 
     internal IEnumerable<byte> Buffer => _buffer.Take(_count);
 
     internal void Add(short sample)
     {
-        if (_buffer.Length > _count)
+        if (_buffer.Count > _count)
         {
             _buffer[_count] = (byte)sample;
             _buffer[_count + 1] = (byte)(sample >> 8);
         }
         else
         {
-            Array.Resize(ref _buffer, _buffer.Length + 50);
-            _buffer[_count] = (byte)sample;
-            _buffer[_count + 1] = (byte)(sample >> 8);
-
-           // _buffer.Add((byte)sample);
-           // _buffer.Add((byte)(sample >> 8));
+           _buffer.Add((byte)sample);
+           _buffer.Add((byte)(sample >> 8));
         }
 
         _count += 2;
@@ -31,6 +26,5 @@ internal sealed class AudioBuffer
     internal void Clear()
     {
         _count = 0;
-        Array.Fill<byte>(_buffer, 0);
     }
 }
