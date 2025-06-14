4f30aac2cd27c7f90902fc43d2b1b91be6ff1e22
voytas <voytas@outlook.com>
2025-06-14 22:44:28 +0100

    Refactor: Replace ReactiveUI with CommunityToolkit.Mvvm in `TapeBlockViewModel`, use `[ObservableProperty]` and streamline property definitions

--- src/Spectron/ViewModels/TapeBlockViewModel.cs
+++ src/Spectron/ViewModels/TapeBlockViewModel.cs
@@ -1,16 +1,11 @@
-using ReactiveUI;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace OldBit.Spectron.ViewModels;
 
-public class TapeBlockViewModel(int? index, string name, string data) : ReactiveObject
+public partial class TapeBlockViewModel(int? index, string name, string data) : ObservableObject
 {
+    [ObservableProperty]
     private bool _isSelected;
-
-    public bool IsSelected
-    {
-        get => _isSelected;
-        set => this.RaiseAndSetIfChanged(ref _isSelected, value);
-    }
 
     public int? Index { get; init; } = index;
     public string Name { get; init; } = name;
