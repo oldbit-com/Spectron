f32b2bf5d3a8993db89bfdfe8d093f76c2669020
voytas <voytas@outlook.com>
2025-09-28 14:01:30 +0100

    Determine disk type

--- src/Spectron.Emulation/Devices/Beta128/Drive/DiskType.cs
+++ src/Spectron.Emulation/Devices/Beta128/Drive/DiskType.cs
@@ -6,4 +6,14 @@ internal static class DiskType
     internal const byte FortyTracksDoubleSided = 0x17;
     internal const byte EightyTracksSingleSided = 0x18;
     internal const byte FortyTracksSingleSided = 0x19;
+
+    internal static byte GetDiskType(int totalTracks, int totalSides) =>
+        (tracks: totalTracks, sides: totalSides) switch
+        {
+            (80, 2) => EightyTracksDoubleSided,
+            (80, 1) => EightyTracksSingleSided,
+            (40, 2) => FortyTracksDoubleSided,
+            (40, 1) => FortyTracksSingleSided,
+            _ => throw new ArgumentException("Invalid disk type"),
+        };
 }
