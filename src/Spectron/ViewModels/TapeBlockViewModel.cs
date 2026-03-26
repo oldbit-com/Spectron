84ed766406e30f8fab40e6390c2f564f4f89c621
voytas <voytas@outlook.com>
2026-03-26 20:50:18 +0000

    Refactor ViewModel properties to use `public partial` for consistency and improved accessibility across models

--- src/Spectron/ViewModels/TapeBlockViewModel.cs
+++ src/Spectron/ViewModels/TapeBlockViewModel.cs
@@ -6,6 +6,7 @@ public partial class TapeBlockViewModel(int? index, string name, string data) : 
 {
     [ObservableProperty]
     public partial bool IsSelected { get; set; }
+
     public int? Index { get; init; } = index;
     public string Name { get; init; } = name;
     public string Data { get; init; } = data;
