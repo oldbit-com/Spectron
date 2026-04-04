9e8690ef7c29d89fe616622434347078a52fda77
voytas <voytas@outlook.com>
2026-04-04 19:49:15 +0100

    Fix test

--- tests/Spectron.Emulator.Tests/Rom/RomReaderTests.cs
+++ tests/Spectron.Emulator.Tests/Rom/RomReaderTests.cs
@@ -9,7 +9,7 @@ public class RomReaderTests
     [InlineData(RomType.Original48, "5ea7c2b824672e914525d1d5c419d71b84a426a2")]
     [InlineData(RomType.Original128Bank0, "4f4b11ec22326280bdb96e3baf9db4b4cb1d02c5")]
     [InlineData(RomType.Original128Bank1, "80080644289ed93d71a1103992a154cc9802b2fa")]
-    [InlineData(RomType.Retroleum, "077df85fb84e9cb466ed9e6fd93f9046ce0c6815")]
+    [InlineData(RomType.Retroleum, "3c907138a57e5750b682c28caf991d596b4eb9ff")]
     [InlineData(RomType.BrendanAlford, "f699b73abfb1ab53c063ac02ac6283705864c734")]
     [InlineData(RomType.BusySoft, "2ee2dbe6ab96b60d7af1d6cb763b299374c21776")]
     [InlineData(RomType.GoshWonderful, "a701c3d4b698f7d2be537dc6f79e06e4dbc95929")]
