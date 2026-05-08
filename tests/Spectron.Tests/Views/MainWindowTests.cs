865f105ac823e13e069ce86f2b2457f6346e5794
voytas <voytas@outlook.com>
2026-05-08 22:44:08 +0100

    Add `ShouldChangeComputerType` test for machine-type menu validation, refactor `ShouldExecuteLoadCommand`, and extract `MachineTypeShouldBeChecked` helper

--- tests/Spectron.Tests/Views/MainWindowTests.cs
+++ tests/Spectron.Tests/Views/MainWindowTests.cs
@@ -1,5 +1,7 @@
+using Avalonia.Controls;
 using Avalonia.Headless.XUnit;
 using CommunityToolkit.Mvvm.Input;
+using OldBit.Spectron.Emulation;
 using OldBit.Spectron.Emulation.Devices.Audio;
 using OldBit.Spectron.ViewModels;
 using OldBit.Spectron.Views;
@@ -38,16 +40,45 @@ public class MainWindowTests : IDisposable
     [AvaloniaFact]
     public void ShouldExecuteLoadCommand()
     {
-        _mainWindow.MainMenu.ShouldNotBeNull();
-
         var loadMenuItem = _mainWindow.FindMenuItem("Load...");
 
         loadMenuItem.ShouldNotBeNull();
         loadMenuItem.Command.ShouldBeOfType<AsyncRelayCommand>();
 
-        ((AsyncRelayCommand)loadMenuItem.Command!).Execute(null);
+        loadMenuItem.Command.Execute(null);
 
         _viewModel.Title.ShouldBe("Spectron - ZX Spectrum Emulator [test.sna]");
         _viewModel.Emulator.ShouldNotBeNull();
+
+        MachineTypeShouldBeChecked("ZX Spectrum 48");
+    }
+
+    [AvaloniaTheory]
+    [InlineData("ZX Spectrum 16", ComputerType.Spectrum16K)]
+    [InlineData("ZX Spectrum 48", ComputerType.Spectrum48K)]
+    [InlineData("ZX Spectrum 128", ComputerType.Spectrum128K)]
+    [InlineData("Timex Computer 2048", ComputerType.Timex2048)]
+    public void ShouldChangeComputerType(string computerName, ComputerType computerType)
+    {
+        _mainWindow.FindMenuItem(computerName)?.Command?.Execute(computerType);
+        _mainWindow.FindMenuItem(computerName)?.IsChecked.ShouldBeTrue();
+
+        _viewModel.Emulator.ShouldNotBeNull();
+        _viewModel.Emulator.ComputerType.ShouldBe(computerType);
+
+        MachineTypeShouldBeChecked(computerName);
+    }
+
+    private void MachineTypeShouldBeChecked(string name)
+    {
+        var machineMenuItem = _mainWindow.FindMenuItem("Machine");
+
+        machineMenuItem.ShouldNotBeNull();
+
+        machineMenuItem.Items.OfType<MenuItem>()
+            .Single(item => Equals(item.Header, name)).IsChecked.ShouldBeTrue();
+
+        machineMenuItem.Items.OfType<MenuItem>()
+            .Where(item => !Equals(item.Header, name)).All(item => item.IsChecked).ShouldBeFalse();
     }
 }
