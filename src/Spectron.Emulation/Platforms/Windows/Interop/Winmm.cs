5e92471fce6f1810b56f93d971a2180e333aeea3
voytas <voytas@outlook.com>
2026-05-19 22:14:43 +0100

    Add comment to clarify Winmm

--- src/Spectron.Emulation/Platforms/Windows/Interop/Winmm.cs
+++ src/Spectron.Emulation/Platforms/Windows/Interop/Winmm.cs
@@ -6,9 +6,20 @@ namespace OldBit.Spectron.Emulation.Platforms.Windows.Interop;
 [SupportedOSPlatform("windows")]
 internal static partial class Winmm
 {
+    /// <summary>
+    /// The timeBeginPeriod function requests a minimum resolution for periodic timers.
+    /// </summary>
+    /// <param name="uPeriod">Minimum timer resolution, in milliseconds, for the application or device driver.
+    /// A lower value specifies a higher (more accurate) resolution.</param>
+    /// <returns>Returns TIMERR_NOERROR if successful or TIMERR_NOCANDO if the resolution specified in uPeriod is out of range.</returns>
     [LibraryImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
     internal static partial uint TimeBeginPeriod(uint uPeriod);
 
+    /// <summary>
+    /// The timeEndPeriod function clears a previously set minimum timer resolution.
+    /// </summary>
+    /// <param name="uPeriod">Minimum timer resolution specified in the previous call to the timeBeginPeriod function.</param>
+    /// <returns>Returns TIMERR_NOERROR if successful or TIMERR_NOCANDO if the resolution specified in uPeriod is out of range.</returns>
     [LibraryImport("winmm.dll", EntryPoint = "timeEndPeriod")]
     internal static partial uint TimeEndPeriod(uint uPeriod);
 }
